cfbc3ee02911aebb099f9fadfe3de90763c8d639
Jonathan Pyrak <bobcatpyrak@gmail.com>
2020-11-16 10:11:14 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { E404Component } from './core/e404/e404.component';
+import { MenuComponent } from './menu/menu.component';
+import { MenuitemComponent } from './menu/menuitem/menuitem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    E404Component,
+    MenuComponent,
+    MenuitemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/system.service.ts.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class System.Service.TsService {
+
+  constructor() { }
+}
